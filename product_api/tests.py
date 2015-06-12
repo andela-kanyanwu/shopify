89240ea0d786d4efce7c09a4b3b9ca00b691ebfd
andela-kanyanwu <kosy.anyanwu@andela.co>
2015-06-12 03:24:31 +0100

    Add tests for category and product

--- product_api/tests.py
+++ product_api/tests.py
@@ -16,6 +16,9 @@ class CategoryTest(APITestCase):
         self.assertEqual(response.data, data)
 
     def test_get_category_when_no_data_is_available(self):
+        """
+            Ensure we get an empty list when no category object is available.
+        """
         Category.objects.all().delete()
         data = []
         response = self.client.get('/categories/')
@@ -23,10 +26,34 @@ class CategoryTest(APITestCase):
 
     def test_post_category(self):
         """
-            Ensure we can create a category objects.
+            Ensure we can create a category object.
         """
         data = {"id": 2, "name": "All", "description": "Available for everyone"}
         response = self.client.post('/categories/', data, format='json')
+        self.assertEqual(response.data, data)
+
+    def test_post_category_with_empty_field(self):
+        """
+            Ensure we cannot post category with empty field.
+        """
+        data = {"name": "", "description": ""}
+        response = self.client.post('/categories/', data, format='json')
+        self.assertEqual(response.data, {'name': ['This field may not be blank.'], 'description': ['This field may not be blank.']})
+
+    def test_delete_category(self):
+        """
+            Ensure we can delete a category object.
+        """
+        data = {"id": 1, "name": "All", "description": "Available for everyone"}
+        response = self.client.delete("/categories/1/")
+        self.assertEqual(response.data, None)
+
+    def test_put_category(self):
+        """
+            Ensure we can update a category object.
+        """
+        data = {'name': [u'This field is required.']}
+        response = self.client.put("/categories/1/")
         self.assertEqual(response.data, data)
 
 
@@ -40,8 +67,27 @@ class ProductTest(APITestCase):
         """
             Ensure we can get all product objects.
         """
-        data = [{"url": "http://testserver/products/1/", "name": "soap", "description": "Best soap", "category": 'http://testserver/categories/1/', "supplier": "http://testserver/suppliers/1/", "price_per_unit": 20, "size": "small", "colour": "white"}]
+        data = [{"url": "http://testserver/products/1/", "name": "soap", "description": "Best soap", "category": "http://testserver/categories/1/", "supplier": "http://testserver/suppliers/1/", "price_per_unit": 20, "size": "small", "colour": "white"}]
 
         response = self.client.get('/products/')
         self.assertEqual(response.data, data)
+
+    def test_post_product(self):
+        """
+            Ensure we can create a product object
+        """
+        category_object = Category.objects.create(name="All", description="Available for everyone")
+        supplier_object = Supplier.objects.create(name="Djangs", address="32 Mac road", city="Lagos", country="Nigeria", email="djangs@yahoo.com", website="www.yahoo.com")
+        data = {"url": "http://testserver/products/2/", "name": "soap", "description": "Best soap", "category": 'http://testserver/categories/2/', "supplier": "http://testserver/suppliers/2/", "price_per_unit": 20, "size": "small", "colour": "white"}
+        response = self.client.post('/products/', data, format='json')
+        self.assertEqual(response.data, data)
+
+    def test_delete_product(self):
+        """
+            Ensure we can delete a product object.
+        """
+        data = [{"url": "http://testserver/products/1/", "name": "soap", "description": "Best soap", "category": "http://testserver/categories/1/", "supplier": "http://testserver/suppliers/1/", "price_per_unit": 20, "size": "small", "colour": "white"}]
+        response = self.client.delete("/products/1/")
+        self.assertEqual(response.data, None) 
+
     
