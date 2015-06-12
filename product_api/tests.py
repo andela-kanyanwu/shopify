faa543c1da01ebb05c8cfaf177114c491654da60
andela-kanyanwu <kosy.anyanwu@andela.co>
2015-06-12 07:54:17 +0100

    Update test for category and product

--- product_api/tests.py
+++ product_api/tests.py
@@ -30,6 +30,7 @@ class CategoryTest(APITestCase):
         """
         data = {"id": 2, "name": "All", "description": "Available for everyone"}
         response = self.client.post('/categories/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data, data)
 
     def test_post_category_with_empty_field(self):
@@ -52,8 +53,8 @@ class CategoryTest(APITestCase):
         """
             Ensure we can update a category object.
         """
-        data = {'name': [u'This field is required.']}
-        response = self.client.put("/categories/1/")
+        data = {"id": 1, "name": "All", "description": "Available for everyone"}
+        response = self.client.put("/categories/1/", data, format='json')
         self.assertEqual(response.data, data)
 
 
@@ -72,22 +73,54 @@ class ProductTest(APITestCase):
         response = self.client.get('/products/')
         self.assertEqual(response.data, data)
 
+    def test_get_product_when_no_data_is_available(self):
+        """
+            Ensure we get an empty list when no product object is available.
+        """
+        Product.objects.all().delete()
+        data = []
+        response = self.client.get('/products/')
+        self.assertEqual(response.data, data)
+
+    def test_get_product_with_primary_key_that_does_not_exist(self):
+        """
+            Ensure we get a 'Not found' data for product with primary key that do not exist
+        """
+        data = {"detail": "Not found."}
+        response = self.client.get('/products/3/')
+        self.assertEqual(response.data, data)
+
     def test_post_product(self):
         """
             Ensure we can create a product object
         """
-        category_object = Category.objects.create(name="All", description="Available for everyone")
-        supplier_object = Supplier.objects.create(name="Djangs", address="32 Mac road", city="Lagos", country="Nigeria", email="djangs@yahoo.com", website="www.yahoo.com")
+        Category.objects.create(name="All", description="Available for everyone")
+        Supplier.objects.create(name="Djangs", address="32 Mac road", city="Lagos", country="Nigeria", email="djangs@yahoo.com", website="www.yahoo.com")
         data = {"url": "http://testserver/products/2/", "name": "soap", "description": "Best soap", "category": 'http://testserver/categories/2/', "supplier": "http://testserver/suppliers/2/", "price_per_unit": 20, "size": "small", "colour": "white"}
         response = self.client.post('/products/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data, data)
 
     def test_delete_product(self):
         """
             Ensure we can delete a product object.
         """
-        data = [{"url": "http://testserver/products/1/", "name": "soap", "description": "Best soap", "category": "http://testserver/categories/1/", "supplier": "http://testserver/suppliers/1/", "price_per_unit": 20, "size": "small", "colour": "white"}]
+        data = {"url": "http://testserver/products/1/", "name": "soap", "description": "Best soap", "category": "http://testserver/categories/1/", "supplier": "http://testserver/suppliers/1/", "price_per_unit": 20, "size": "small", "colour": "white"}
         response = self.client.delete("/products/1/")
         self.assertEqual(response.data, None) 
 
-    
+    def test_delete_product_with_primary_key_that_does_not_exist(self):
+        """
+            Ensure we cannot delete a product with primary key that does not exist.
+        """
+        data = {"detail": "Not found."}
+        response = self.client.delete("/products/4/")
+        self.assertEqual(response.data, data)
+
+    def test_put_product(self):
+        """
+            Ensure we can update a product object.
+        """
+        data = {"url": "http://testserver/products/1/", "name": "soap", "description": "Best soap", "category": "http://testserver/categories/1/", "supplier": "http://testserver/suppliers/1/", "price_per_unit": 20, "size": "small", "colour": "white"}
+        response = self.client.put("/products/1/", data, format='json')
+        self.assertEqual(response.data, data)
